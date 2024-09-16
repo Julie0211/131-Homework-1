d4da0b363ef57f8159da6f32af5396d8777203e7
Julie Araujo <109998693+Julie0211@users.noreply.github.com>
2024-09-16 03:30:14 +0000

    new file:   .vscode/tasks.json 	new file:   Build.sh 	new file:   GroceryItem.cpp 	new file:   GroceryItem.hpp 	deleted:    homework 	new file:   main.cpp 	new file:   output.txt 	new file:   sample_input.txt 	new file:   sample_output.txt

---
+++ Build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+clang++ -std=c++20 -Wall -Wextra -Werror -o GroceryItem main.cpp GroceryItem.cpp
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <memory>
+#include <vector>
+
+#include "GroceryItem.hpp"
+
+int main()
+{
+	GroceryItem g;
+
+	std::vector<std::unique_ptr<GroceryItem>> groceries;
+	while(std::cin >> g)
+	{
+		groceries.push_back(std::make_unique<GroceryItem>(g));
+	}
+
+	for (auto it = groceries.rbegin(); it != groceries.rend(); ++it)
+	{
+		std::cout << *(it->get()) << std::endl;
+	}
+	
+	return 0;
+}
